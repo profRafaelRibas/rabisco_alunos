3702748caf55dd87948af3afc875f70376743812
Rafael Ribas <133046889+profRafaelRibas@users.noreply.github.com>
2024-05-24 16:28:00 -0300

    240520241627

---
+++ pages/produtos.jsx
@@ -0,0 +1,11 @@
+import Headerb from '../components/Headerb'
+import Titulo from '../components/Titulo'
+
+export default function produtos() {
+    return (
+        <>
+            <Headerb />
+            <Titulo texto="Conheça nossos produtos!"/>
+        </>
+    )
+}
